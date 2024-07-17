bd33051d7849595471fc58376226d058db859683
gajjoshi <gajbjoshi@gmail.com>
2024-07-18 01:34:24 +0530

    first commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,12 +6,33 @@ module.exports = {
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+    container: {
+      center: true,
+      padding: "15px",
+      screens: {
+        sm: "640px",
+        md: "768px",
+        lg: "960px",
+        xl: "1200px",
       },
+
+      
+    
+    },
+    fontFamily:{
+      primary: "var(--font-jetbrainsMono)"
+    },
+    extend: {
+      colors:{
+
+        primary:"#1c1c22",
+        accent:{
+          DEFAULT:"#00ff99",
+          hover:"#00e187",
+      
+        },
+      },
+      
     },
   },
   plugins: [],
